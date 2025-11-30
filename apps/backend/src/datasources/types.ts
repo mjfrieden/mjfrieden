386bd36af491c5592828392d6c23ab8e53758e4a
Marshall <144085481+mjfrieden@users.noreply.github.com>
2025-11-30 10:21:51 -0600

    Delete apps directory

--- apps/backend/src/datasources/types.ts
+++
@@ -1,8 +0,0 @@
-import { FhirImmunization } from '@whitecloud/fhir-types';
-
-export interface ImmunizationDataSource {
-  id: string;
-  displayName: string;
-  connect(userId: string): Promise<void>;
-  fetchImmunizations(userId: string): Promise<FhirImmunization[]>;
-}
